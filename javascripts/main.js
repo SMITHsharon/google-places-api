4191a5654566b7f8dc024ca0afd8681bac053142
sharon smith <sharon.e.smith2@gmail.com>
2017-04-25 00:09:27 -0500

    output clean and styled / one question

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,24 +3,50 @@ $(document).ready(function(){
 
 // DON'T PUSH API KEY UP TO GITHUB
 const apiKey = "";
-
+let menuChoice;
 
 // writes the result of the call to the DOM
 const writeToDOM = (ajaxObject) => {
 
 	domString = "";
-
 	let placesArray = ajaxObject.results;
 
-	placesArray.forEach(function(thisPlace) {
-		domString += `${thisPlace.name}`;
-	})
+	$("#outputHere").empty(); // clear the DOM of prior output
+
+	domString += `<div class="resultHeader">`;
+	if (menuChoice === "liquor_store") {
+		domString += `Search Matches for Liquor Stores`;
+	} else 
+	if (menuChoice === "mosque") {
+		domString += `Search Matches for Mosques`;
+	} else 
+	if (menuChoice === "night_club") {
+		domString += `Search Matches for Night Clubs`;
+	} else 
+	if (menuChoice === "cemetery") {
+		domString += `Search Matches for Cemeteries`;
+	} else {
+		domString += `Search Matches for Fire Stations`;
+	}
+	domString += `</div>`;
+
+	if (placesArray.length !== 0) {
+		placesArray.forEach(function(thisPlace) {
+			domString += `<div class="thisPlace">`;
+			domString += `${thisPlace.name}</div>`;
+		})
+	} else {
+		domString += 
+			domString += `<div class="thisPlace">There were no places found matching your request.</div>`;
+	}
 
 	$("#outputHere").append(domString);
 };
 
+
 // event handler on menu click
 $("body").on("click", "li", (e) => {
+	menuChoice = e.target.innerHTML;
 	loadPlaces(e.target.innerHTML).then((data) => {
 	}).catch((error) => {
 		console.log(error);
