4a32bbf1cfce14e659c4bbc9cc567ac86ec9669f
sharon smith <sharon.e.smith2@gmail.com>
2017-04-24 21:32:26 -0500

    adding class code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,36 @@
-console.log("Javascript loading");
+
+$(document).ready(function(){
+
+const apiKey = "";
+
+$("body").on("click", "li", () => {
+	loadPlaces(e.target.innerHTML).then((data) => {
+		console.log("data");
+	}).catch((error) => {
+		console.log(error);
+	});
+});
+
+const loadPlaces = (dropdownType) => {
+
+		return new Promise ((resolve, reject) => {
+			$.ajax("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=50000&type=${dropdownType}&keyword=cruise&key=${apiKey}")
+		.done((data) =>  resolve(data))
+		.fail((error) => reject(error));
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
